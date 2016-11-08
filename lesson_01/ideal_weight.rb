a3b83002f7df73db9939280d2a4cb531f75421d6
Elias <igolubevssmnu@mail.ru>
2016-11-08 18:56:09 +0300

    Create programm ideal weight(ideal_weight.rb)

---
+++ lesson_01/ideal_weight.rb
@@ -0,0 +1,17 @@
+# !/usr/bin/env ruby
+# coding: utf-8
+# ideal weight
+print "[Console] Введите имя: "
+name = gets.chomp
+name.capitalize!
+
+print "[Console] Введите ваш рост: "
+height = Integer(gets.chomp)
+
+ideal_weight = height - 110
+
+if ideal_weight <= 0
+	puts "[Console] Ваш вес уже оптимальный"
+else
+	puts "[Console] #{name}, ваш идеальный вес #{ideal_weight}"
+end
