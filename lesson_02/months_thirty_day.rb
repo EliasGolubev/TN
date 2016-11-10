ab4ff0dee0c3aaed83c3792081584a0ba131d617
Elias <igolubevssmnu@mail.ru>
2016-11-10 21:09:32 +0300

    Rename day_case --> day_box, and month_case --> month_box

--- lesson_02/months_thirty_day.rb
+++ lesson_02/months_thirty_day.rb
@@ -10,16 +10,16 @@ month_hash = {}
 
 (1..12).each do |month|  
   if month != 2  
-    Date.valid_date?(year, month, 31) ? day_case.push(31) : day_case.push(30)
+    Date.valid_date?(year, month, 31) ? day_box.push(31) : day_box.push(30)
   else
-    Date.leap?(year) ? day_case.push(29) : day_case.push(28)
+    Date.leap?(year) ? day_box.push(29) : day_box.push(28)
   end
 end
  
-day_case.each_index do |index| 
-  month_hash[month_case[index].to_sym] = { day: day_case[index] }
+day_box.each_index do |index| 
+  month_hash[month_box[index].to_sym] = { day: day_box[index] }
 end
 
 month_hash.each do |key, value| 
-  puts "[Console] #{key} : #{value[:day]}" if value[:day] == 30
+  puts "[Console] #{key} : #{value[:day]} day" if value[:day] == 30
 end
