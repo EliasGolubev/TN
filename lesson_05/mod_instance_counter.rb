a2eb03c1c596b2d72d8af6ef5f6c70f5ca858956
Elias <igolubevssmnu@mail.ru>
2016-12-04 11:55:36 +0300

    Style correct

--- lesson_05/mod_instance_counter.rb
+++ lesson_05/mod_instance_counter.rb
@@ -1,24 +1,23 @@
 module InstanceCounter
-  
   @@count = 0
-  
+
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
   end
 
   module ClassMethods
-    
     def instances
-      self.class_variable_get(:@@count)
-    end  
+      class_variable_get(:@@count)
+    end
   end
 
-  module InstanceMethods   
-    private 
+  module InstanceMethods
+    private
+
     def register_instance
       count = self.class.class_variable_get(:@@count) + 1
-      self.class.class_variable_set(:@@count, count)  
+      self.class.class_variable_set(:@@count, count)
     end
   end
 end
