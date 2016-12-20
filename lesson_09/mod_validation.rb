b0a725836f0d6ca44e0b2c54b52a685e9c2e2c4a
Elias <igolubevssmnu@mail.ru>
2016-12-20 22:39:54 +0300

    Fixed Validation module

--- lesson_09/mod_validation.rb
+++ lesson_09/mod_validation.rb
@@ -5,67 +5,59 @@ module Validation
   end
 
   module ClassMethods
-  #
-  # Содержит метод класса validate. 
-  # Этот метод принимает в качестве параметров имя проверяемого атрибута, 
-  # а также тип валидации и при необходимости дополнительные параметры.
-  # 
-    def validate(par = {}, &block)
-      #
-      # format (при этом отдельным параметром задается регулярное выражение для формата). 
-      # Треубет соответствия значения атрибута заданному регулярному выражению. 
-      #
-      if block_given?      
-       raise "Name error" if block.call
-      else
-        #
-        # presence - требует, чтобы значение атрибута было не nil 
-        # и не пустой строкой.
-        #
-        if par.include?(:name)
-          raise "Presence error" if par[:name] == nil || par[:name] == ""
-        #
-        # type (третий параметр - класс атрибута). 
-        # Требует соответствия значения атрибута заданному классу
-        #
-        elsif par.include?(:number) && par.include?(:type)
-          raise "Type error" if par[:number].class.to_s != par[:type]
-        end   
-      end
-    end  
+    attr_reader :validate_par
+    
+    def validate (args={})
+      @validate_par ||= []
+      def_hash = deafault_validate_format
+      temp_hash = def_hash.merge(args)
+      @validate_par << {name: temp_hash[:name], type: temp_hash[:type]}
+    end
+
+    def deafault_validate_format
+      {name: 'deafault', type: 'deafault'}
+    end
   end
 
   module InstanceMethods
-    
-    #
-    # Содержит инстанс-метод valid? который возвращает true, 
-    # если все проверки валидации прошли успешно и false, если есть ошибки валидации.
-    #
-    def valid?(par = {}, regexp)
-      validate!(par, regexp)
+
+    def valid?
+      validate!
       true
     rescue
       false
-    end  
+    end
 
-    private
-    
-    #   
-    # Содержит инстанс-метод validate!, который запускает все проверки (валидации), 
-    # указанные в классе через метод класса validate.
-    # В случае ошибки валидации выбрасывает исключение с 
-    # сообщением о том, какая именно валидация не прошла
-    #
-    def validate!(par = {}, regexp=nil)
-      if par.include?(:name)
-        self.class.validate :name => par[:name]
-      elsif par.include?(:number) && par.include?(:type)
-        self.class.validate :number => par[:number], :type => par[:type]
-      elsif par.include?(:station)         
-        self.class.validate :station => par[:station] { par[:station] !~ regexp }
-      else
-        raise "No parameters"
+    private 
+
+    def validate!(par=nil)
+      if !self.class.validate_par.nil?
+        self.class.validate_par.each do |parameters|
+          presence(parameters[:name]) if parameters[:type] == 'presence'
+          format(parameters[:name], par) if parameters[:type] == 'format'
+          type(parameters[:name], par) if parameters[:type] == 'type'
+        end
       end
-    end  
-  end 
+    end
+
+    def presence name
+      var = get_valid_variables(name)
+      raise "Presence error" if var.nil? || var.to_s.empty?
+    end
+
+    def format (name, reg)
+      var = get_valid_variables(name)
+      raise "Format error" if var !~ reg
+    end
+
+    def type(name, parameter)
+      var = get_valid_variables(name)
+      raise "Type error" if var.class != parameter
+    end
+
+    def get_valid_variables(name)
+      var_name = "@#{name}".to_sym
+      var = instance_variable_get(var_name)
+    end
+  end
 end
